2e550f86858e641361fe1eb534be996109249cb6
tomi19sdz <123742@student.san.edu.pl>
2025-08-09 20:00:12 +0200

    Dodanie footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-400 py-4 text-center mt-10">
+      <p>© {new Date().getFullYear()} TypyApp. Wszystkie prawa zastrzeżone.</p>
+    </footer>
+  );
+}
