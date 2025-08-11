071db933819c187520b9b057a14083a678ffbcf4
tomi19sdz <123742@student.san.edu.pl>
2025-08-11 19:54:08 +0200

    Dodanie poprawionego route.ts

--- src/app/api/fetch-data/route.ts
+++ src/app/api/fetch-data/route.ts
@@ -1,7 +1,5 @@
 // src/app/api/fetch-data/route.ts
 import { NextResponse } from 'next/server';
-import { promises as fs } from 'fs';
-import path from 'path';
 
 export async function GET() {
   const apiKey = process.env.FOOTBALL_API_KEY; 
@@ -15,6 +13,7 @@ export async function GET() {
   try {
     const res = await fetch('https://v3.football.api-sports.io/fixtures?date=2024-05-15', {
       headers: {
+        // Poprawne użycie klucza API z zmiennej środowiskowej
         'x-rapidapi-key': apiKey,
         'x-rapidapi-host': 'v3.football.api-sports.io'
       },
@@ -23,23 +22,25 @@ export async function GET() {
 
     if (!res.ok) {
       console.error('Błąd z API. Status:', res.status, 'StatusText:', res.statusText);
-      const errorData = await res.json().catch(() => null); // Upewniamy się, że to nie zawiesza kodu
+      const errorData = await res.json().catch(() => null);
       return NextResponse.json({ error: 'Failed to fetch data from API', apiError: errorData }, { status: res.status });
     }
 
     const data = await res.json();
     
-    // Upewniamy się, że dane są w odpowiednim formacie
     if (!data || !data.response) {
       console.error('API zwróciło nieoczekiwane dane:', data);
       return NextResponse.json({ error: 'Unexpected data format from API' }, { status: 500 });
     }
 
-    // Zapisz dane do pliku w folderze public
-    const filePath = path.join(process.cwd(), 'public', 'cached-data.json');
-    await fs.writeFile(filePath, JSON.stringify(data.response, null, 2));
-
-    return NextResponse.json({ message: 'Data fetched and cached successfully' });
+    // Zwracamy dane z nagłówkiem Cache-Control.
+    // To poinstruuje Vercel, aby przechowywał odpowiedź w pamięci podręcznej przez 3600 sekund (1 godzina).
+    // W ten sposób, następne wywołania tego endpointu przez 1 godzinę nie będą obciążać API-Sports.
+    return NextResponse.json(data.response, {
+      headers: {
+        'Cache-Control': 's-maxage=3600, stale-while-revalidate'
+      }
+    });
 
   } catch (error) {
     console.error('Wewnętrzny błąd serwera:', error);
