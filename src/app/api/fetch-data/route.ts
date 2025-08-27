7a9f7fb6a59fcab2fcd8166c7be2030c6bac11e6
tomi19sdz <123742@student.san.edu.pl>
2025-08-27 19:52:32 +0200

    zmiana route 1

--- src/app/api/fetch-data/route.ts
+++ src/app/api/fetch-data/route.ts
@@ -3,38 +3,30 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const apiKey = process.env.FOOTBALL_API_KEY; 
-  // Poniższa tablica zawiera ID lig do pobrania z API.
-  // Możesz dodawać lub usuwać ID, aby dostosować widoczne ligi.
   const leagueIds = [
-    // English Premier League
     '39',
-    // Polish Ekstraklasa
     '106', 
-    // Spanish La Liga
     '140',
-    // German Bundesliga
     '78',
-    // Italian Serie A
     '135',
-    // French Ligue 1
     '61',
-    // UEFA Champions League
     '2',
-    // UEFA Europa League
     '3',
-    // UEFA Europa Conference League
     '848'
   ];
   const today = new Date().toISOString().split('T')[0];
+  const apiUrl = `https://v3.football.api-sports.io/fixtures?date=${today}&league=${leagueIds.join(',')}`;
 
+  // Logowanie URL-a, aby sprawdzić, czy jest poprawny
+  console.log('Pobieram dane z URL-a:', apiUrl);
+  
   if (!apiKey) {
     console.error('API key not found');
     return NextResponse.json({ error: 'API key not found' }, { status: 500 });
   }
 
   try {
-    // API-Sports pozwala na wyszukiwanie meczów po dacie i ID ligi
-    const res = await fetch(`https://v3.football.api-sports.io/fixtures?date=${today}&league=${leagueIds.join(',')}`, {
+    const res = await fetch(apiUrl, {
       headers: {
         'x-rapidapi-key': apiKey,
         'x-rapidapi-host': 'v3.football.api-sports.io'
